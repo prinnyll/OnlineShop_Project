a21e5e702af9109a623341caea9cc4796891ac38
Johnny <johnnyffaaaa@gmail.com>
2025-01-27 14:35:00 +0800

    Update MongoDB connection URI in session configuration

--- config/session.js
+++ config/session.js
@@ -4,7 +4,8 @@ const mongoDbStore = require("connect-mongodb-session");
 const createSessionStore = () => {
   const MongoDBStore = mongoDbStore(expressSession);
   const store = new MongoDBStore({
-    uri: "mongodb://127.0.0.1:27017",
+    //uri: "mongodb://127.0.0.1:27017",
+    uri: `mongodb+srv://johnnyffaa:q4s9j6lMAzkaxEV2@cluster0.8tsva.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
     databaseName: "online-shop",
     collection: "sessions",
   });

5fe086e3063bc931b03ec727b6e17c2bf2ae5623
Johnny <johnnyffaaaa@gmail.com>
2025-01-27 12:18:48 +0800

    Refactor MongoDB URL handling in database configuration

--- data/database.js
+++ data/database.js
@@ -2,13 +2,19 @@ const mongodb = require("mongodb");
 
 const MongoClient = mongodb.MongoClient;
 
-const url =
-  "mongodb+srv://johnnyffaa:<9NbLylSgwshLGk1A>@cluster0.sssjw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// const url =
+//   "mongodb+srv://johnnyffaa:<9NbLylSgwshLGk1A>@cluster0.sssjw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-let mongodbUrl = "mongodb://127.0.0.1:27017";
+// let mongodbUrl = "mongodb://127.0.0.1:27017";
 
-if (process.env.MONGODB_URL) {
-  url = process.env.MONGODB_URL;
+// if (process.env.MONGODB_URL) {
+//   mongodbUrl = process.env.MONGODB_URL;
+// }
+let mongodbUrl = process.env.MONGODB_URL;
+
+if (!mongodbUrl) {
+  mongodbUrl =
+    "mongodb+srv://johnnyffaa:9NbLylSgwshLGk1A@cluster0.sssjw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 }
 
 let database;

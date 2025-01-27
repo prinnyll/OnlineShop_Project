a4c0aaa96772561182424e721df22351f5d111c4
Johnny <johnnyffaaaa@gmail.com>
2025-01-27 13:25:24 +0800

    Update MongoDB URL in database configuration

--- data/database.js
+++ data/database.js
@@ -2,19 +2,18 @@ const mongodb = require("mongodb");
 
 const MongoClient = mongodb.MongoClient;
 
-// const url =
-//   "mongodb+srv://johnnyffaa:<9NbLylSgwshLGk1A>@cluster0.sssjw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// const url = `mongodb+srv://johnnyffaa:q4s9j6lMAzkaxEV2@cluster0.8tsva.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // let mongodbUrl = "mongodb://127.0.0.1:27017";
 
 // if (process.env.MONGODB_URL) {
 //   mongodbUrl = process.env.MONGODB_URL;
 // }
-let mongodbUrl = process.env.MONGODB_URL;
 
-if (!mongodbUrl) {
-  mongodbUrl =
-    "mongodb+srv://johnnyffaa:9NbLylSgwshLGk1A@cluster0.sssjw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+let mongodbUrl = `mongodb+srv://johnnyffaa:q4s9j6lMAzkaxEV2@cluster0.8tsva.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+if (process.env.MONGODB_URL) {
+  mongodbUrl = process.env.MONGODB_URL;
 }
 
 let database;
